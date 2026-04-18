2557927655c3e04ff2527411e8421c3f509433af
Sabinzz <sabin.poudel014@gmail.com>
2026-04-18 11:19:40 +0545

    Infinte scrolling added

--- my-project/src/Pagination/Page.tsx
+++ my-project/src/Pagination/Page.tsx
@@ -1,4 +1,4 @@
-import  { useContext } from 'react'
+import  { useContext, useEffect } from 'react'
 import { movieContext } from '../Context/MovieContext'
 
 const Page = () => {
@@ -6,14 +6,21 @@ const Page = () => {
     if(!pageContext){
         throw new Error("Cant get different page content")
     }
+     
     const{page,setpage}=pageContext
+    useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }, [page])
   return (
     <div className='flex gap-3 justify-center mt-5 pb-3 items-center'>
         <div>
 <button
 onClick={()=>{if(page>1){
 setpage((prev) => prev - 1)
- window.scrollTo({ top: 0, behavior: "smooth" })
+
 }}}
 className='border border-gray-400 p-2 cursor-pointer hover:bg-[#79C142] hover:text-white rounded-md px-3 text-(--text) text-lg'
 >Prev</button>
@@ -25,7 +32,7 @@ className='border border-gray-400 p-2 cursor-pointer hover:bg-[#79C142] hover:te
             <button
            onClick={() => {
   setpage((prev) => prev + 1)
-  window.scrollTo({ top: 0, behavior: "smooth" })
+
 }}
             className='border border-gray-400 p-2 rounded-md px-3 cursor-pointer hover:text-white hover:bg-[#79C142] text-(--text) text-lg '>
                 Next
